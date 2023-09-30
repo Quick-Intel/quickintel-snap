c46f381eda33b08919c05aedf79cec62a033e1ed
Z-Dev <anthony@quickintel.io>
2023-09-30 15:47:49 -0600

    initial sync

---
+++ packages/site/src/pages/index.tsx
@@ -0,0 +1,217 @@
+import { useContext } from 'react';
+import styled from 'styled-components';
+import { MetamaskActions, MetaMaskContext } from '../hooks';
+import {
+  connectSnap,
+  getSnap,
+  isLocalSnap,
+  sendHello,
+  shouldDisplayReconnectButton,
+} from '../utils';
+import {
+  ConnectButton,
+  InstallFlaskButton,
+  ReconnectButton,
+  SendHelloButton,
+  Card,
+} from '../components';
+import { defaultSnapOrigin } from '../config';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  margin-top: 7.6rem;
+  margin-bottom: 7.6rem;
+  ${({ theme }) => theme.mediaQueries.small} {
+    padding-left: 2.4rem;
+    padding-right: 2.4rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    width: auto;
+  }
+`;
+
+const Heading = styled.h1`
+  margin-top: 0;
+  margin-bottom: 2.4rem;
+  text-align: center;
+`;
+
+const Span = styled.span`
+  color: ${(props) => props.theme.colors.primary.default};
+`;
+
+const Subtitle = styled.p`
+  font-size: ${({ theme }) => theme.fontSizes.large};
+  font-weight: 500;
+  margin-top: 0;
+  margin-bottom: 0;
+  ${({ theme }) => theme.mediaQueries.small} {
+    font-size: ${({ theme }) => theme.fontSizes.text};
+  }
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 64.8rem;
+  width: 100%;
+  height: 100%;
+  margin-top: 1.5rem;
+`;
+
+const Notice = styled.div`
+  background-color: ${({ theme }) => theme.colors.background.alternative};
+  border: 1px solid ${({ theme }) => theme.colors.border.default};
+  color: ${({ theme }) => theme.colors.text.alternative};
+  border-radius: ${({ theme }) => theme.radii.default};
+  padding: 2.4rem;
+  margin-top: 2.4rem;
+  max-width: 60rem;
+  width: 100%;
+
+  & > * {
+    margin: 0;
+  }
+  ${({ theme }) => theme.mediaQueries.small} {
+    margin-top: 1.2rem;
+    padding: 1.6rem;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  background-color: ${({ theme }) => theme.colors.error.muted};
+  border: 1px solid ${({ theme }) => theme.colors.error.default};
+  color: ${({ theme }) => theme.colors.error.alternative};
+  border-radius: ${({ theme }) => theme.radii.default};
+  padding: 2.4rem;
+  margin-bottom: 2.4rem;
+  margin-top: 2.4rem;
+  max-width: 60rem;
+  width: 100%;
+  ${({ theme }) => theme.mediaQueries.small} {
+    padding: 1.6rem;
+    margin-bottom: 1.2rem;
+    margin-top: 1.2rem;
+    max-width: 100%;
+  }
+`;
+
+const Index = () => {
+  const [state, dispatch] = useContext(MetaMaskContext);
+
+  const isMetaMaskReady = isLocalSnap(defaultSnapOrigin)
+    ? state.isFlask
+    : state.snapsDetected;
+
+  const handleConnectClick = async () => {
+    try {
+      await connectSnap();
+      const installedSnap = await getSnap();
+
+      dispatch({
+        type: MetamaskActions.SetInstalled,
+        payload: installedSnap,
+      });
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
+  const handleSendHelloClick = async () => {
+    try {
+      await sendHello();
+    } catch (e) {
+      console.error(e);
+      dispatch({ type: MetamaskActions.SetError, payload: e });
+    }
+  };
+
+  return (
+<Container>
+      <Heading>
+        Welcome to <Span>Token Audit by Quick Intel</Span>
+      </Heading>
+      <Subtitle>
+        <code>V0.1.1</code>
+      </Subtitle>
+      <CardContainer>
+        {state.error && (
+          <ErrorMessage>
+            <b>An error happened:</b> {state.error.message}
+          </ErrorMessage>
+        )}
+        {!isMetaMaskReady && (
+          <Card
+            content={{
+              title: 'Install',
+              description:
+                'Snaps is pre-release software only available in MetaMask Flask, a canary distribution for developers with access to upcoming features.',
+              button: <InstallFlaskButton />,
+            }}
+            fullWidth
+          />
+        )}
+        {!state.installedSnap && (
+          <Card
+            content={{
+              title: 'Connect',
+              description:
+                'Connect to install the latest version of Token Audit by Quick Intel snap.',
+              button: (
+                <ConnectButton
+                  onClick={handleConnectClick}
+                  disabled={!isMetaMaskReady}
+                />
+              ),
+            }}
+            disabled={!isMetaMaskReady}
+          />
+        )}
+        {shouldDisplayReconnectButton(state.installedSnap) && (
+          <Card
+            content={{
+              title: 'Reconnect',
+              description:
+                'While connected to a local running snap this button will always be displayed in order to update the snap if a change is made.',
+              button: (
+                <ReconnectButton
+                  onClick={handleConnectClick}
+                  disabled={!state.installedSnap}
+                />
+              ),
+            }}
+            disabled={!state.installedSnap}
+          />
+        )}
+        <Card
+          content={{
+            title: 'Start Token Audit Protection',
+            description:
+              `Congratulations! Whenever you go to swap tokens on a DEX, be sure to review the Quick Intel tab and get a quick analysis of the token.`,
+          }}
+          disabled={!state.installedSnap}
+          fullWidth={
+            isMetaMaskReady &&
+            Boolean(state.installedSnap) &&
+            !shouldDisplayReconnectButton(state.installedSnap)
+          }
+        />
+        <Notice>
+          <p>
+            Web 3 users rely on companies like MetaMask for their Web 3 wallet needs. Many scams occur in the space, with not much insight due to the nature
+            of Web 3. Quick Intel Snap is a tool designed for all MetaMask users, to provide a layer of security. Powered by Quick Intel, you can quickly see a wide variety of 
+            potential risks before investing in a token. With Token Audit by Quick Intel Snap, you'll be able to stay a step ahead and protect yourself.
+          </p>
+        </Notice>
+      </CardContainer>
+    </Container>
+  );
+};
+
+export default Index;
---
+++ packages/snap/src/index.ts
@@ -0,0 +1,112 @@
+import { OnTransactionHandler } from '@metamask/snaps-types';
+import { divider, heading, panel, text, copyable } from '@metamask/snaps-ui';
+import { QUICK_INTEL_URL, QUICK_INTEL_KEY } from '../quickintel/constants';
+
+// Handle outgoing transactions.
+export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+  const quickIntelShield = '🛡';
+
+  const chainId = await ethereum.request({ method: 'eth_chainId' });
+
+  const txData = transaction?.data;
+
+  const body = {
+    chainid: chainId,
+    txdata: txData?.toString(),
+  };
+
+  const auditReq = await getData(body);
+  console.log('sending tkns', txData, transaction?.data);
+  const panelArr: any = [];
+  // Display Token Audit details.
+  if (
+    (typeof transaction.data === 'string' && transaction.data === '0x') ||
+    txData === undefined
+  ) {
+    return {
+      content: panel([
+        heading(`${quickIntelShield} Quick Intel Audit`),
+        text(
+          'This snap only provides transaction insights for tokens when interacting with contracts (ie. DEX Swaps.)',
+        ),
+      ]),
+    };
+  }
+
+  if (auditReq?.length > 0) {
+    auditReq.forEach((item: any) => {
+      panelArr.push(
+        text(
+          `Token: ${item?.tokenName} (${
+            item?.tokenSymbol
+          })\n\n\nContract Verified: ${
+            item?.verified ? '✅' : '❌'
+          }\n\n\nWarnings: ${item?.warnings}${
+            item?.warning_Details?.length > 0
+              ? `\n\n\n${item?.warning_Details
+                  ?.map((itm: any) => {
+                    return `${itm}\n\n\n`;
+                  })
+                  .join('')}`
+              : ''
+          }\n\n\nCautions: ${item?.cautions}${
+            item?.caution_Details?.length > 0
+              ? `\n\n\n${item?.caution_Details
+                  ?.map((itm: any) => {
+                    return `${itm}\n\n\n`;
+                  })
+                  .join('')}`
+              : ''
+          }`,
+        ),
+        divider(),
+        text(`View Full Audit on Quick Intel:`),
+        copyable(
+          `https://app.quickintel.io/scanner?type=token&chain=${item?.chain}&contractAddress=${item?.tokenAddress}`,
+        ),
+      );
+    });
+  } else {
+    throw new Error(
+      `No Audit Results or chain not supported for Token Audit. Contact Quick Intel for more information.`,
+    );
+  }
+
+  if (panelArr.length === 0) {
+    panelArr.push(
+      text(
+        `No Audit Results or chain not supported for Token Audit. Contact Quick Intel for more information.`,
+      ),
+    );
+  }
+
+  return {
+    content: panel([
+      heading(`${quickIntelShield} Quick Intel Token Audit`),
+      ...panelArr,
+    ]),
+  };
+};
+
+/**
+ * Request Audit
+ *
+ * @param bodydata
+ * @returns Array.
+ */
+async function getData(bodydata: object | null) {
+  console.log('tst', QUICK_INTEL_KEY, QUICK_INTEL_URL);
+  const response = await fetch(
+    `${QUICK_INTEL_URL}`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        api_key: QUICK_INTEL_KEY,
+      },
+      body: JSON.stringify(bodydata),
+    },
+  );
+  return response?.json();
+}
